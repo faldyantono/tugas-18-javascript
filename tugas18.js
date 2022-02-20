fe5d52049476bafb6f843f4d76b1f17c74ec3e18
faldyantono <96801457+faldyantono@users.noreply.github.com>
2022-02-21 03:04:31 +0700

    Add files via upload

---
+++ tugas18.js
@@ -0,0 +1,5 @@
+function kalimat() {
+  var x = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+  console.log(x.slice(8, 24));
+}
+kalimat()
